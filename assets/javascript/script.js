9ea4b6c38d7e2dc05faddb4be35a584b0bf9b131
Rashmi Sharma <rksharma2679@gmail.com>
2020-02-01 21:21:15 -0500

    Working search

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,31 +1,52 @@
 var APIKey = "2a29d28596f5b969578c8d643d9842b5";
-var city = "Austin";
+var city = "Apex";
 // The URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + APIKey;
+
 var queryUVI = "https://api.openweathermap.org/data/2.5/uvi?appid=" + APIKey;
-var currentDay = moment().format("MMM Do YY"); 
+var currentDay = moment().format('L'); 
+
+init();
 
 // We then created an AJAX call for temperature humidity and windspeed
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    var imgCode = response.weather[0].icon;
-    var iconurl = "https://openweathermap.org/img/wn/" + imgCode + ".png";
-    var img = $("<img>").attr("src", iconurl);
-    $(".cityDate").text(response.name + " (" + currentDay + ")");
-    $(".cityDate").append(img);
-    //var FahrenheitTemp = Number((response.main.temp - 273.15) * 1.80 +32);
-    $(".temperature").html("Temperature: " + response.main.temp + '&#8451');
-    $(".humidity").text("Humidity: " + response.main.humidity);
-    $(".windSpeed").text("Wind Speed: " + response.wind.speed);
-    queryUVI = queryUVI + "&lat=" + response.coord.lat + "&lon=" +response.coord.lon;
-    UVIndex(queryUVI);
-});
-// "http://api.openweathermap.org/data/2.5/uvi?appid=2a29d28596f5b969578c8d643d9842b5&lat=19.01&lon=72.85"
+function init(){
+    fiveDayForecast();
+    cityDetails();
+    // searchHistory();
+}
+
+//When clicked on search for cities
+function searchCity() {
+    city = $("#searchText").val().trim();
+    cityDetails();
+}
+
+//function to display city details
+function cityDetails() {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(data) {
+        var imgCode = data.weather[0].icon;
+        var iconurl = "https://openweathermap.org/img/wn/" + imgCode + ".png";
+        var img = $("<img>").attr("src", iconurl);
+        $(".cityDate").text(data.name + " (" + currentDay + ")");
+        $(".cityDate").append(img);
+        //var FahrenheitTemp = Number((response.main.temp - 273.15) * 1.80 +32);
+        $(".temperature").html("Temperature: " + data.main.temp + '&#8451');
+        $(".humidity").text("Humidity: " + data.main.humidity);
+        $(".windSpeed").text("Wind Speed: " + data.wind.speed);
+        queryUVI = queryUVI + "&lat=" + data.coord.lat + "&lon=" +data.coord.lon;
+        console.log(queryUVI);
+        UVIndex(queryUVI);
+        
+    });
+}
+
 //AJAX call for getting UV Index
 function UVIndex(uviUrl) {
-    $.ajax({
+    return $.ajax({
+        
         url: uviUrl,
         method: "GET"
         }).then(function(res) {
@@ -33,5 +54,14 @@ function UVIndex(uviUrl) {
             $(".uvIndex").text("UV Index: " + res.value);
         });
 }
-    
-    
+
+//Display five day forecast
+function fiveDayForecast() {
+    for(var i = 0; i < 5; i++) {
+        $(".date"+i).text(currentDay);
+    }
+}
+
+function searchHistory(){
+
+}
